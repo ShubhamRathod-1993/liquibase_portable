4e5ac7194439df8b215c91f27575aa8da9eb3327
ShubhamRathod-1993 <57096292+ShubhamRathod-1993@users.noreply.github.com>
2020-10-14 14:43:25 +0530

    d

--- liquibase/files/orcl/changelog.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset Bob:1
-CREATE TABLE customers_liquibase 
-( customer_id number(10) NOT NULL,  
-  customer_name varchar2(50) NOT NULL,  
-  city varchar2(50),  
-  CONSTRAINT customers_pk PRIMARY KEY (customer_id)  
-);  
